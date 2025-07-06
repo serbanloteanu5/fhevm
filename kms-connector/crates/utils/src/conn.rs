7261b63b6ac2c51f569f0d8ad95dca59cc6278d1
serbanloteanu5 <146815475+serbanloteanu5@users.noreply.github.com>
2025-07-06 10:46:24 +0200

    Update conn.rs

--- kms-connector/crates/utils/src/conn.rs
+++ kms-connector/crates/utils/src/conn.rs
@@ -3,29 +3,21 @@ use alloy::{
     network::EthereumWallet,
     providers::{
         Identity, ProviderBuilder, ProviderLayer, RootProvider, WsConnect,
-        fillers::{
-            BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller, TxFiller,
-            WalletFiller,
-        },
+        fillers::{BlobGasFiller, ChainIdFiller, FillProvider, GasFiller, JoinFill, NonceFiller, TxFiller, WalletFiller},
     },
 };
 use anyhow::anyhow;
 use sqlx::{Pool, Postgres, postgres::PgPoolOptions};
 use std::time::Duration;
-use tracing::{info, warn};
+use tracing::{info,warn};
 
-/// The number of connection retry to connect to the database or the Gateway RPC node.
 pub const CONNECTION_RETRY_NUMBER: usize = 5;
-
-/// The delay between two connection attempts.
 pub const CONNECTION_RETRY_DELAY: Duration = Duration::from_secs(2);
 
-/// Tries to establish the connection with Postgres database.
 pub async fn connect_to_db(db_url: &str, db_pool_size: u32) -> anyhow::Result<Pool<Postgres>> {
+    let options = PgPoolOptions::new().max_connections(db_pool_size);
     for i in 1..=CONNECTION_RETRY_NUMBER {
         info!("Attempting connection to DB... ({i}/{CONNECTION_RETRY_NUMBER})");
-
-        let options = PgPoolOptions::new().max_connections(db_pool_size);
         match options.connect(db_url).await {
             Ok(db_pool) => {
                 info!("Connected to Postgres database successfully");
@@ -33,7 +25,6 @@ pub async fn connect_to_db(db_url: &str, db_pool_size: u32) -> anyhow::Result<Po
             }
             Err(e) => warn!("DB connection attempt #{i} failed: {e}"),
         }
-
         if i != CONNECTION_RETRY_NUMBER {
             tokio::time::sleep(CONNECTION_RETRY_DELAY).await;
         }
@@ -41,59 +32,52 @@ pub async fn connect_to_db(db_url: &str, db_pool_size: u32) -> anyhow::Result<Po
     Err(anyhow!("Could not connect to Postgres DB at url {db_url}"))
 }
 
-/// The default `Filler`s for an `alloy::Provider`.
 type DefaultFillers = JoinFill<
     Identity,
-    JoinFill<GasFiller, JoinFill<BlobGasFiller, JoinFill<NonceFiller, ChainIdFiller>>>,
->;
+    JoinFill<GasFiller,
+             JoinFill<BlobGasFiller,
+                      JoinFill<NonceFiller,
+                               ChainIdFiller>>>>;
 
-/// The default `alloy::Provider` used to interact with the Gateway.
 pub type GatewayProvider = FillProvider<DefaultFillers, RootProvider>;
-
-/// The default `alloy::Provider` used to interact with the Gateway using a wallet.
 pub type WalletGatewayProvider =
     FillProvider<JoinFill<DefaultFillers, WalletFiller<EthereumWallet>>, RootProvider>;
 
-/// Tries to establish the connection with a RPC node of the Gateway.
-pub async fn connect_to_gateway(gateway_url: &str) -> anyhow::Result<GatewayProvider> {
-    connect_to_gateway_inner(gateway_url, ProviderBuilder::new).await
-}
-
-/// Tries to establish the connection with a RPC node of the Gateway, with a `WalletFiller`.
-pub async fn connect_to_gateway_with_wallet(
+async fn connect_to_gateway_inner<L,F>(
     gateway_url: &str,
-    wallet: KmsWallet,
-) -> anyhow::Result<WalletGatewayProvider> {
-    connect_to_gateway_inner(gateway_url, || {
-        ProviderBuilder::new().wallet(wallet.clone())
-    })
-    .await
-}
-
-/// Tries to establish the connection with a RPC node of the Gateway.
-async fn connect_to_gateway_inner<L, F>(
-    gateway_url: &str,
-    provider_builder_new: impl Fn() -> ProviderBuilder<L, F>,
+    provider_builder_new: impl Fn() -> ProviderBuilder<L,F>,
 ) -> anyhow::Result<F::Provider>
 where
-    L: ProviderLayer<RootProvider>,
-    F: ProviderLayer<L::Provider> + TxFiller,
+   L: ProviderLayer<RootProvider>,
+   F: ProviderLayer<L::Provider> + TxFiller
 {
-    for i in 1..=CONNECTION_RETRY_NUMBER {
-        info!("Attempting connection to Gateway... ({i}/{CONNECTION_RETRY_NUMBER})");
+   for i in 1..=CONNECTION_RETRY_NUMBER {
+       info!("Attempting connection to Gateway... ({i}/{CONNECTION_RETRY_NUMBER})");
+       let ws_endpoint = WsConnect::new(gateway_url);
+       match provider_builder_new().on_ws(ws_endpoint).await {
+           Ok(provider) => {
+               info!("Connected to Gateway's RPC node successfully");
+               return Ok(provider);
+           }
+           Err(e) => warn!("Gateway connection attempt #{i} failed: {e}"),
+       }
+       if i != CONNECTION_RETRY_NUMBER {
+           tokio::time.sleep(CONNECTION_RETRY_DELAY).await;
+       }
+   }
+   Err(anyhow!("Could not connect to Gateway at url {gateway_url}"))
+}
 
-        let ws_endpoint = WsConnect::new(gateway_url);
-        match provider_builder_new().on_ws(ws_endpoint).await {
-            Ok(provider) => {
-                info!("Connected to Gateway's RPC node successfully");
-                return Ok(provider);
-            }
-            Err(e) => warn!("Gateway connection attempt #{i} failed: {e}"),
-        }
+pub async fn connect_to_gateway(gateway_url:&str)->anyhow.Result<GatewayProvider>{
+   connect_to_gateway_inner(gateway_url , ProviderBuilder::<Identity,_>::new ).await
+}
 
-        if i != CONNECTION_RETRY_NUMBER {
-            tokio::time::sleep(CONNECTION_RETRY_DELAY).await;
-        }
-    }
-    Err(anyhow!("Could not connect to Gateway at url {gateway_url}"))
+pub async fn connect_to_gateway_with_wallet(
+     gateway_url:&str ,
+     wallet : KmsWallet ,
+ ) -> anyhow.Result <WalletGatewayProvider> 
+{
+     connect_to_gateway_inner(gateway_url , ||{
+         ProviderBuilder::<_, _>::new().wallet(wallet.clone())
+     }).await
 }
